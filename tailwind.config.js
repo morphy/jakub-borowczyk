0d26ebe48a044d1ccb964dc9e7926dda7ace3788
morphy <36381325+morphy@users.noreply.github.com>
2025-05-28 06:41:41 +0200

    move files to src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./_includes/**/*.{md,markdown,liquid,html,svg}",
-    "./_layouts/**/*.{md,markdown,liquid,html,svg}",
-    "./_pages/**/*.{md,markdown,liquid,html,svg}",
-    "./_posts/**/*.{md,markdown,liquid,html,svg}",
-    "./_articles/**/*.{md,markdown,liquid,html,svg}",
-    "./_data/picture.yml",
-    "./assets/js/*.*"
+    "./src/_includes/**/*.{md,markdown,liquid,html,svg}",
+    "./src/_layouts/**/*.{md,markdown,liquid,html,svg}",
+    "./src/_pages/**/*.{md,markdown,liquid,html,svg}",
+    "./src/_posts/**/*.{md,markdown,liquid,html,svg}",
+    "./src/_articles/**/*.{md,markdown,liquid,html,svg}",
+    "./src/_data/picture.yml",
+    "./src/assets/js/*.*"
   ],
   safelist: ["bg-opacity-40"]
 };
